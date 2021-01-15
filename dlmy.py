e07cafec86402c6271a71a564e72e3ccba87c313
yrwq <yrwqid@gmail.com>
2021-01-16 00:06:54 +0100

    update

--- dlmy.py
+++ dlmy.py
@@ -1,13 +1,66 @@
 #!/usr/bin/env python3
 from __future__ import unicode_literals
 import requests
+import urllib.request
+import json
 import sys
 import getopt
 import youtube_dl
 import time
-from search import yt_search
-from colors import col
 from bs4 import BeautifulSoup
+
+class yt_search:
+    """
+    Search on youtube with a title.
+    Returns: youtube url
+    """
+    def __init__(self, search_terms: str):
+        self.search_terms = search_terms
+        self.videos = self.search()
+
+    def search(self):
+        encoded_search = urllib.parse.quote(self.search_terms)
+        BASE_URL = "https://youtube.com"
+        url = f"{BASE_URL}/results?search_query={encoded_search}"
+        response = requests.get(url).text
+        while "ytInitialData" not in response:
+            response = requests.get(url).text
+        results = self.parse_html(response)
+        return results
+
+    def parse_html(self, response):
+        results = []
+        start = (
+            response.index("ytInitialData")
+            + len("ytInitialData")
+            + 3
+        )
+        end = response.index("};", start) + 1
+        json_str = response[start:end]
+        data = json.loads(json_str)
+
+        videos = data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"][
+            "sectionListRenderer"
+        ]["contents"][0]["itemSectionRenderer"]["contents"]
+
+        for video in videos:
+            res = {}
+            if "videoRenderer" in video.keys():
+                video_data = video.get("videoRenderer", {})
+                res["title"] = video_data.get("title", {}).get("runs", [[{}]])[0].get("text", None)
+                res["url_suffix"] = video_data.get("navigationEndpoint", {}).get("commandMetadata", {}).get("webCommandMetadata", {}).get("url", None)
+                results.append(res)
+        return results
+
+    def to_dict(self):
+        return self.videos
+
+class col:
+    blue = '\033[94m'
+    ok = '\033[92m'
+    warn = '\033[93m'
+    fail = '\033[91m'
+    end = '\033[0m'
 
 def spotify_track(url):
     """
@@ -37,8 +90,6 @@ def spotify_playlist(url):
 
     return songs
 
-def prompt(ask):
-    return str(input(f"{col.ok}{ask} {col.warn}(Y/n){col.end}"))
 
 # Youtube Download options
 ydl_opts = {
@@ -47,6 +98,8 @@ ydl_opts = {
     'noplaylist': True,
     'continue_dl': True,
     'restrictfilenames': True,
+    'quiet': True,
+    'no_warnings': True,
     'postprocessors': [
         {
             'key': 'FFmpegExtractAudio',
@@ -99,11 +152,13 @@ def main():
                     print(f'\n{col.blue}{results[0]["title"]}' + col.end + "\n")
                     url_suffix = results[0]["url_suffix"]
 
-                    prompt = prompt("Is this correct?")
+                    prompt = str(input(f"{col.ok}Is this correct? {col.warn}(Y/n){col.end}"))
 
                     if prompt == "n" or prompt == "N":
                         exit(1)
                     else:
+                        print("")
+                        print(f"{col.warn}Downloading: {col.blue}{results[0]['title']}{col.end}")
                         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                             ydl.download(["http://www.youtube.com" + url_suffix])
 
@@ -125,16 +180,18 @@ def main():
 
                     print("")
 
-                    prompt = prompt("Is this correct?")
+                    prompt = str(input(f"{col.ok}Is this correct? {col.warn}(Y/n){col.end}"))
 
                     if prompt == "n" or prompt == "N":
                         exit(1)
 
                     else:
+                        print("")
                         for tag in songs:
                             title = spotify_track(tag["content"])
                             results = yt_search(title).to_dict()
                             url_suffix = results[0]["url_suffix"]
+                            print(f"{col.warn}Downloading: {col.blue}{results[0]['title']}{col.end}")
                             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                                 ydl.download(["http://www.youtube.com" + url_suffix])
 
@@ -147,3 +204,4 @@ def main():
     except KeyboardInterrupt:
         exit(1)
 
+main()
--- dlmy/colors.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-class col:
-    blue = '\033[94m'
-    ok = '\033[92m'
-    warn = '\033[93m'
-    fail = '\033[91m'
-    end = '\033[0m'
